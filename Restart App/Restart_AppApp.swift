3ef3137f051199819d04316a8cae8b49cb1c1c85
Abid Mahdi <abid588@outlook.com>
2022-12-27 17:39:03 +0000

    Initial Commit

---
+++ Restart App/Restart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_AppApp.swift
+//  Restart App
+//
+//  Created by Abid Mahdi on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Restart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
